03c92ad09853cb57ffc8dd84d066492fe83b4830
sanjay <sunny4321nita@gmail.com>
2021-06-14 11:05:01 +0530

    string pattern questions

--- src/com/gs/string/GroupAnagram.java
+++ src/com/gs/string/GroupAnagram.java
@@ -1,4 +1,39 @@
 package com.gs.string;
 
+import java.util.*;
+
 public class GroupAnagram {
+    public static void main(String[] args) {
+        System.out.println(group(Arrays.asList(new String[]{"cat", "dog", "god"})));
+    }
+
+    private static Set<Set<String>> group(List<String> words){
+        Map<String, Set<String>> result = new HashMap<>();
+        Set<Set<String>> ans = new HashSet<>();
+
+        for (String word : words) {
+            StringBuffer sb = new StringBuffer();
+            int[] charCount = new int[26];
+            for (char c : word.toCharArray()) {
+                charCount[c-'a']++;
+            }
+            for (int i=0;i<26;i++) {
+                sb.append(charCount[i]);
+            }
+
+            if (result.containsKey(sb.toString())) {
+                result.get(sb.toString()).add(word);
+            } else {
+                Set<String> set = new HashSet<>();
+                set.add(word);
+                result.put(sb.toString(), set);
+            }
+        }
+        for (Set<String> r : result.values()) {
+            ans.add(r);
+        }
+
+        return ans;
+    }
+
 }
--- src/com/gs/string/PenGram.java
+++ src/com/gs/string/PenGram.java
@@ -1,4 +1,28 @@
 package com.gs.string;
 
 public class PenGram {
+    public static void main(String[] args) throws Exception {
+        System.out.println("bfgjkvz".equals(findMissingLetters("The slow purple oryx meanders past the quiescent canine")));
+    }
+
+    private static String findMissingLetters(String sentence) throws Exception {
+        if (sentence==null) {
+            throw  new Exception("sentence is null");
+        }
+        sentence = sentence.toLowerCase();
+        boolean[] isFound = new boolean[26];
+        for (char c : sentence.toCharArray()) {
+            if (c>='a' && c <= 'z' && !isFound[c-'a']) {
+                isFound[c-'a'] = true;
+            }
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i=0;i<26;i++) {
+            if (!isFound[i]) {
+                sb.append((char) ('a' + i));
+            }
+        }
+        return sb.toString();
+    }
 }
