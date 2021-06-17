28d49e98858bb930110fe14a0cc32dedb986f35c
sanjay <sunny4321nita@gmail.com>
2021-06-17 10:03:38 +0530

    dp and miscellaneous questions

--- src/com/gs/dp/StudentElection.java
+++ src/com/gs/dp/StudentElection.java
@@ -1,33 +1,22 @@
 package com.gs.dp;
 
-import com.gs.util.Node;
-
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
 public class StudentElection {
     public static void main(String[] args) {
         System.out.println(whoIsElected(1, 1));
-        System.out.println(whoIsElected(2, 2));
+        System.out.println(whoIsElected(2, 1));
         System.out.println(whoIsElected(4, 2));
+        System.out.println(whoIsElected(5, 2));
     }
 
     private static int whoIsElected(int n, int k) {
-        List<Integer> list = new LinkedList<>();
-        for (int i=1;i<=n;i++) {
-            list.add(i);
-        }
-        int c=n;
-        int pos=k-1;
-        while (c!=1) {
-            pos = pos%c;
-            list.remove(pos);
-            pos+=k;
+        int[] dp =new int[n];
+        dp[0]=0;
+
+        for (int i=1;i<n;i++) {
+            dp[i]=(dp[i-1]+k)%(i+1);
         }
 
-        return list.get(0);
+        return dp[n-1]+1;
     }
 
 }
